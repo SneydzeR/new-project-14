b4d6d202b2bf22345adc6ad1ab8abe7e3a67a6bf
Oleksandr Yarmak <Sneydfarm@gmail.com>
2024-04-13 10:44:20 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,40 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+export default defineConfig(({ command }) => ({
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+  root: 'src',
+  resolve: {
+    alias: {
+      'accordion-js': 'node_modules/accordion-js/dist/accordion.min.js'
+    }
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; 
+          }
+        },
+        entryFileNames: 'assets/js/[name].[hash].js' 
+      },
     },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
+    outDir: '../dist', 
+  },
+  plugins: [
+    injectHTML({
+      inject: {
+        data: {
+          title: 'My App',
+          injectScript: '<script src="/assets/js/main.js"></script>', 
         },
       },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
-});
+    }),
+    FullReload(['./src/**/*.html'], { always: true }) 
+  ],
+}));
